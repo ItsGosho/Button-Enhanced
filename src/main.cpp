17fa8f9311d9713296c4e804bf14c115efcddb1f
itsgosho <itsgosho2@gmail.com>
2022-10-03 21:45:44 +0300

    Description of the algo hold state checkings

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,26 @@ unsigned long holdThresholdMS = 150;
 unsigned long holdNotificationLastMS = 0;
 unsigned long holdNotificationMS = 500;
 
+/**
+ * When a button is kept hold we don't want to inform the user that fast.
+ * Suppose the user decided that he want to receive the notification when a button is pressed each 500 ms.
+ * He would want that if he had a game that requires moving a object while a button is pressed.
+ * Each 500 ms he receives notification he will move the object with 1 pixel.
+ * Changing the notification delay will increase/decrease the smoothness of the movement.
+ */
+bool isHoldNotificationTimePassed() {
+    return (millis() - holdNotificationLastMS) >= holdNotificationMS;
+}
+
+/**
+ * When a button is activated we do not know if there was a single fast click or it is kept hold.
+ * We must define separation between these two phases.
+ * The @param holdThresholdMS defines after what activation/pressing time the button is in hold phase.
+ */
+bool isEnteredHold() {
+    return timeMS >= holdThresholdMS;
+}
+
 void loop() {
 
     int buttonState = digitalRead(BUTTON_PIN);
@@ -30,7 +50,7 @@ void loop() {
     if (buttonState && startMS > 0) { // Intermediate. Still not released, but pressed.
         timeMS = millis() - startMS;
 
-        if (timeMS >= holdThresholdMS && (millis() - holdNotificationLastMS) >= holdNotificationMS) {
+        if (isEnteredHold() && isHoldNotificationTimePassed()) {
             Serial.println("Hold");
             holdNotificationLastMS = millis();
         }
